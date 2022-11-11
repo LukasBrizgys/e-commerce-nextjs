fa25fd46283a6dc430f36b41816176357c8e58cf
Lukas Brizgys <lukenis456@gmail.com>
2022-11-11 23:00:32 +0200

    add cart actions

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -1,11 +1,19 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { CartComponent } from "../types/CartComponent.type";
-import axios, { AxiosError, AxiosResponse } from 'axios';
+import { createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import { CartComponent, CartComponentBase } from "../types/CartComponent.type";
+import axios, { AxiosResponse } from 'axios';
 import { RootState } from "../store";
 import { HYDRATE } from "next-redux-wrapper";
 export interface IAddSuccessful {
     status: string,
     message: CartComponent
+}
+export interface IRemoveSuccessful {
+    status: string,
+    message: CartComponentBase
+}
+export interface IUpdateSuccessful {
+    status: string,
+    message: CartComponentBase
 }
 export interface IGetSuccessful {
     status:string,
@@ -15,9 +23,15 @@ export interface IResponseFailed {
     status:string,
     message: string
 }
-export type ComponentToAdd = {
-    componentId : number,
+export type ComponentAdd = { 
+    componentId : number;
     quantity : number
+}
+export type ComponentUpdate = ComponentAdd
+export type ComponentToAddOrUpdate = {
+    componentId : number,
+    quantity : number,
+    fallbackQuantity? : number
 }
 export type CartState = {
     components: CartComponent[]
@@ -33,14 +47,32 @@ const initialState : CartState = {
 }
 export const addToCart = createAsyncThunk(
     'cart/addToCart',
-    async({componentId, quantity} : ComponentToAdd) => {
+    async({componentId, quantity} : ComponentToAddOrUpdate) => {
             const response : AxiosResponse<IAddSuccessful> = await axios.post('/api/cartComponent',{componentId, quantity});
             return response.data;
         
 
     }
 )
-const calculateTotalPrice = (items : CartComponent[]) => items.reduce((acc, current) => acc + current.Component.Pricing[0].price,0)
+export const deleteCartItem = createAsyncThunk(
+    'cart/deleteCartItem',
+    async( componentId : number, thunkAPI ) => {
+        const response : AxiosResponse<IRemoveSuccessful> = await axios.delete(`/api/cartComponent/${componentId}`);
+        return response.data;
+    }
+)
+export const setQuantity = createAsyncThunk<IUpdateSuccessful, ComponentUpdate, {rejectValue : ComponentUpdate}>(
+    'cart/setQuantity',
+    async({ componentId, quantity} : ComponentUpdate, { rejectWithValue}) => {
+        try{
+            const response : AxiosResponse<IUpdateSuccessful> = await axios.patch(`/api/cartComponent/${componentId}/quantity/${quantity}`)
+            return response.data;
+        }catch(error) {
+            return rejectWithValue({ componentId, quantity});
+        }
+    }
+)
+const calculateTotalPrice = (items : CartComponent[]) => items.reduce((acc, current) => acc + current.Component.Pricing[0].price * current.quantity,0)
 const calculateTotalQuantity = (items : CartComponent[]) => items.reduce((acc, current) => acc + current.quantity,0)
 export const getCartItems = createAsyncThunk('cart/getCartItems', 
     async() => {
@@ -71,27 +103,55 @@ export const cartSlice = createSlice({
             state.totalPrice = calculateTotalPrice(state.components);
             state.totalQuantity = calculateTotalQuantity(state.components);
             state.loading = false;
-        }),
-        builder.addCase(addToCart.rejected, (state, action) => {
+        })
+        .addCase(addToCart.rejected, (state  : CartState) => {
             state.loading = false;
         })
-        builder.addCase(addToCart.pending, (state) => {
+        .addCase(addToCart.pending, (state : CartState) => {
             state.loading = true;
         })
-        builder.addCase(getCartItems.fulfilled, (state, action : PayloadAction<IGetSuccessful>) => {
+        .addCase(getCartItems.fulfilled, (state : CartState, action : PayloadAction<IGetSuccessful>) => {
             state.components = action.payload.message;
             state.totalPrice = calculateTotalPrice(action.payload.message);
             state.totalQuantity = calculateTotalQuantity(action.payload.message);
             state.loading = false;
         })
-        builder.addCase(getCartItems.pending, (state) => {
+        .addCase(getCartItems.pending, (state : CartState) => {
+            
             state.loading = true;
         })
-        builder.addCase(getCartItems.rejected, (state) => {
+        .addCase(getCartItems.rejected, (state : CartState) => {
             state.loading = false;
+        })
+        .addCase(deleteCartItem.fulfilled, (state : CartState, action : PayloadAction<IRemoveSuccessful>) => {
+            state.components = state.components.filter((component : CartComponent) => component.componentId !== action.payload.message.componentId);
+            state.totalPrice = calculateTotalPrice(state.components);
+            state.totalQuantity = calculateTotalQuantity(state.components);
+            state.loading = false;
+        })
+        .addCase(deleteCartItem.rejected, (state : CartState) => {
+            state.loading = false;
+        })
+        .addCase(deleteCartItem.pending, (state : CartState) => {
+            state.loading = true;
+        })
+        .addCase(setQuantity.fulfilled, (state : CartState, action : PayloadAction<IUpdateSuccessful>) => {
+            const index = state.components.findIndex((component : CartComponent) => component.componentId === action.payload.message.componentId);
+            state.components[index].quantity = action.payload.message.quantity;
+            state.totalPrice = calculateTotalPrice(state.components);
+            state.totalQuantity = calculateTotalQuantity(state.components);
+            state.loading = false;
+        })
+        .addCase(setQuantity.pending, (state : CartState) => {
+            state.loading = true;
+        })
+        .addCase(setQuantity.rejected, (state: CartState) => {
+            state.loading = false;
+        })
+        .addDefaultCase((state) => {
+            state = initialState;
         })
     }
 })
-const reducer = cartSlice.reducer;
 export const selectCart = (state : RootState) => state.cart;
 export default cartSlice.reducer;
