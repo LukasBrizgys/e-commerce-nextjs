7d4a345f79bff3e2fb0d0ec7b739af042856015e
Lukas Brizgys <lukenis456@gmail.com>
2022-11-24 23:00:01 +0200

    move session management to middleware

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,8 @@ import { Category } from '@prisma/client'
 import { createClient, PostgrestResponse } from '@supabase/supabase-js'
 import type { GetServerSideProps, GetServerSidePropsContext, NextPage, InferGetServerSidePropsType } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css';
-import crypto from 'crypto';
-import { getCookie, setCookie} from 'cookies-next';
 export const getServerSideProps : GetServerSideProps = async(ctx : GetServerSidePropsContext) => {
   const supabase = createClient(process.env.NEXT_SUPABASE_API_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
-  const sessionCookie = getCookie('sessionId', ctx);
-  if(!sessionCookie) {
-    let expiryDate = new Date();
-    const month = (expiryDate.getMonth() + 1) % 12;
-    expiryDate.setMonth(month);
-    setCookie('sessionId', crypto.randomUUID(),{ req:ctx.req, res:ctx.res, expires: expiryDate, httpOnly:true, sameSite:'lax', path:'/*'});
-  }
   const { data: categories } : PostgrestResponse<Category> = await supabase.from('Category').select('*');
   return {
     props: {
@@ -23,9 +13,8 @@ export const getServerSideProps : GetServerSideProps = async(ctx : GetServerSide
 }
 const Home: NextPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        
         <title>Kompiuterio komponentų El. parduotuvė</title>
         <meta property="og:title" content="Kompiuterio komponentų El. parduotuvė" key="title" />
         <meta charSet='UTF-8'/>
