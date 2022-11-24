0896df5bd93c230a0be47d4591cc5c7c41760c8a
Lukas Brizgys <lukenis456@gmail.com>
2022-11-24 23:04:42 +0200

    component filtering logic added

--- pages/kategorija/[categorySlug]/index.tsx
+++ pages/kategorija/[categorySlug]/index.tsx
@@ -1,21 +1,43 @@
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { Category } from "@prisma/client";
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { PostgrestResponse } from "@supabase/postgrest-js";
 import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
-import { getCategoryComponentsBySlug } from "../../../src/db/queries/component.queries";
+import { findComponents, findMaxPriceInCategory } from "../../../src/db/queries/component.queries";
 import Head from 'next/head';
 import CatalogItem from '../../../components/CatalogItem';
 import { FullComponent } from '../../../src/types/Component.types';
+import Slider from '../../../components/Slider';
+import Loader from '../../../components/Loader';
+import { useRouter } from 'next/router';
 export const getServerSideProps : GetServerSideProps = async(ctx : GetServerSidePropsContext) => {
     const supabase = createServerSupabaseClient(ctx);
+    let minPrice : number | undefined;
+    let maxPrice : number | undefined;
+    let brand : string[] | undefined;
+
+    if(ctx.query.minPrice) minPrice = parseInt(ctx.query.minPrice.toString()) * 100;
+    if(ctx.query.maxPrice) maxPrice = parseInt(ctx.query.maxPrice.toString()) * 100;
+    if(ctx.query.brand) brand = ctx.query.brand.toString().split(','); 
+    const maxCategoryPrice = await findMaxPriceInCategory(ctx.params?.categorySlug?.toString()!);
     const { data: categories, error: categoryError } : PostgrestResponse<Category> = await supabase.from('Category').select('*');
-    const components : FullComponent[] | null = await getCategoryComponentsBySlug(ctx.params!.categorySlug!.toString());
+    const components : FullComponent[] | null = await findComponents({
+      categorySlug:ctx.params?.categorySlug?.toString(),
+      minPrice:minPrice,
+      maxPrice: maxPrice,
+      brand: brand
+    });
     if(components) {
       return {
         props: {
           categories,
-          components: components
+          ...(minPrice && {minPrice}),
+          ...(maxPrice && {maxPrice}),
+          maxCategoryPrice,
+          components: JSON.parse(JSON.stringify(
+            components,
+            (key, value) => (typeof value === 'bigint' ? value.toString() : value) // return everything else unchanged
+          ))
         }
     }
     }
@@ -28,25 +50,41 @@ export const getServerSideProps : GetServerSideProps = async(ctx : GetServerSide
   }
   
 const Catalog = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const [components, setComponents] = useState([]);
-  useEffect(() => {
-    setComponents(props.components);
-  },[props.components])
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [minPrice, setMinPrice] = useState<number>(props.minPrice / 100);
+  const [maxPrice, setMaxPrice] = useState<number>(props.maxPrice / 100);
+  const router = useRouter();
+  const refreshData = () => {
+      router.replace({
+      query:{...router.query, minPrice:minPrice, maxPrice:maxPrice}
+    });
+  }
 
+  useEffect(() => {
+    setIsLoading(false);
+  },[props.components]);
+
+  useEffect(() => {
+    refreshData();
+    setIsLoading(true);
+  },[minPrice,maxPrice]);
   return (
       <>
         <Head>
             <title>Katalogas</title>
             <meta property="og:title" content="Katalogas" key="title"/>
         </Head>
-        <div className="flex justify-between">
-          <div className="basis-14 fixed">
-              <h3>Filtrai</h3>
+        <div className="flex">
+          <div className="mt-12 mx-5 flex  flex-col items-center p-10 bg-gray-100 rounded-md self-start">
+              <Slider min={0} max={Math.ceil(props.maxCategoryPrice / 100)} currentMax={maxPrice} setMinPrice={setMinPrice} setMaxPrice={setMaxPrice}/>
           </div>
-          <div className="flex justify-center gap-4 flex-wrap items-stretch">
+          <div className="flex gap-6 justify-center flex-wrap items-stretch">
             {
-              components && components.map((component : FullComponent) => 
+              props.components && !isLoading && props.components.map((component : FullComponent) => 
               (<CatalogItem key={component.id} component={component}/>))
+            }
+            {
+              isLoading && <Loader className="w-20 h-20  absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"/>
             }
           </div>
         </div>
