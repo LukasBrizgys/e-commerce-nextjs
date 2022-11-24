35561f646e5a70e4ea820d31117084a2ab5fb959
Lukas Brizgys <lukenis456@gmail.com>
2022-11-24 23:04:14 +0200

    add method to exclude layout in some pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,44 +1,54 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout';
-import React, { useEffect, useState } from 'react';
+import React, { PropsWithChildren, useState } from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../src/store';
-import { AppProps } from 'next/app';
 import { Category } from '@prisma/client';
 import { Suspense } from 'react';
 import dynamic from 'next/dynamic';
 import { createBrowserSupabaseClient, Session } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import Loader from '../components/Loader';
+import Alert from '../components/Alert';
+import { NextRouter, useRouter } from 'next/router';
+import { NextComponentType, NextPageContext } from 'next';
+import { AppPropsType } from 'next/dist/shared/lib/utils';
+import { AppProps } from 'next/app';
 interface CustomAppProps {
-  categories: Category[],
-  initialSession : Session
+  Component : NextComponentType<NextPageContext<any>>;
+  pageProps : any;
+  categories: Category[];
+  userEmail?: string;
+  initialSession: Session,
+  appProps:PropsWithChildren<AppPropsType<NextRouter, {}>>;
 }
-const DynamicLoginModal = dynamic(() => import('../components/LoginModal'), { suspense:true });
-const DynamicRegisterModal = dynamic(() => import('../components/RegisterModal'), { suspense: true })
-function MyApp({ Component, pageProps} : any) {
+const DynamicLoginModal = dynamic(() => import('../components/LoginModal'), { suspense:true, ssr:true });
+const DynamicRegisterModal = dynamic(() => import('../components/RegisterModal'), { suspense: true, ssr:true})
+function MyApp({ Component, pageProps} : AppProps<CustomAppProps>) {
   const [ supabaseClient ] = useState(() => createBrowserSupabaseClient());
-  useEffect(() => {
-    const getSessionCookie = async() => {
-      await fetch('/api/session',{method:'post'})
+  const router = useRouter();
+  const getGlobalLayout = () => {
+    if(['/checkout/success', '/checkout'].includes(router.pathname)) return <Component {...pageProps}/>
+    return (
+      <Layout categories={pageProps.categories}>
+          <Component {...pageProps}/>{" "}
+      </Layout>
+    )
     }
-    getSessionCookie();
-  }, [])
   return (
     <>
     <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
       <Provider store={store}>
-        <Layout categories={pageProps.categories}>
-              <Component {...pageProps}/>{" "}
-        </Layout>
+        {getGlobalLayout()}
         <Suspense fallback={<Loader/>}>
           <DynamicLoginModal/>
         </Suspense>
         <Suspense fallback={<Loader/>}>
           <DynamicRegisterModal/>
         </Suspense>
+        <Alert/>
       </Provider>
-    </SessionContextProvider>  
+    </SessionContextProvider>
     </>
   )
   
