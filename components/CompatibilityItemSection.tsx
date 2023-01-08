e7dd92637d04f0d6846cfa6bf36b21c5a5451a75
Lukas Brizgys <lukenis456@gmail.com>
2023-01-08 23:02:31 +0200

    add more info

--- components/CompatibilityItemSection.tsx
+++ components/CompatibilityItemSection.tsx
@@ -25,7 +25,7 @@ const CompatibilityItemSection = (props : ISection) => {
             <Image src={process.env.NODE_ENV === 'development' ? `/${props.selectedItem.ComponentPicture[0].Picture.name}` : 'https://njkmajcfosaflafhlphb.supabase.co/storage/v1/object/public/pictures/' + props.selectedItem.ComponentPicture[0].Picture.name} width={75} height={75} alt={props.selectedItem.name}/>
             <div>
                 <div>{props.selectedItem.name}</div>
-                <svg onClick={() => setOpen(!open)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12 absolute cursor-pointer left-1/2">
+                <svg onClick={() => setOpen(!open)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={open ? "w-12 h-12 absolute cursor-pointer left-1/2 rotate-180" : "w-12 h-12 absolute cursor-pointer left-1/2"}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                 </svg>
             </div>
@@ -34,12 +34,28 @@ const CompatibilityItemSection = (props : ISection) => {
         {
             open &&
             <div className="flex w-full border rounded items-stretch justify-center p-2 gap-5">
-                {props.selectedItem.ComponentFeature.map((entry) => (
-                    <div className="flex flex-col w-full" key={entry.Feature.name}>
-                        <div className="w-full text-sm text-center">{entry.Feature.name}</div>
-                        <div className="w-14 h-24 text-center text-ellipsis text-sm overflow-hidden">{entry.value}</div>
-                    </div>
-                ))}
+                <table className="table-fixed w-full text-center">
+                    <thead>
+                        <tr>
+
+                        {
+                            props.selectedItem.ComponentFeature.map((entry) => (
+                                <th className="border">{entry.Feature.name}</th>
+                            ))
+                        }
+                       </tr> 
+                    </thead>
+                    <tbody>
+                        <tr>
+                            {
+                                props.selectedItem.ComponentFeature.map((entry) => (
+                                    <td className="text-ellipsis break-words border">{entry.value}</td>
+                                ))
+                            }
+                        </tr>
+                    </tbody>
+                </table>
+                
             </div>
         }
 
